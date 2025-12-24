dc735a6864a858b899021c255f6bd5c8b59cc266
Kunal Singh <kunalindia59@gmail.com>
2025-12-24 22:36:34 +0530

    Added /cart to top level router

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import userRouter from "./user.js";
 import orderRouter from "./order.js";
 import productsRouter from "./product.js";
+import cartRouter from "./cart.js";
 
 const router = express.Router();
 router.use("/user", userRouter);
 router.use("/order", orderRouter);
 router.use("/product", productsRouter);
+router.use('/cart', cartRouter);
 
 export default router;
