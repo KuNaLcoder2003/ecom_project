a31a526c05c01b83934cf11975553449409131ff
Kunal Singh <kunalindia59@gmail.com>
2025-12-27 15:38:40 +0530

    A hook to get details of product

---
+++ frontend/src/hooks/useProduct.tsx
@@ -0,0 +1,51 @@
+
+import { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom";
+
+interface ProductImage {
+    id: string;
+    image_url: string;
+    product_id: string;
+}
+
+interface Product {
+    id: string;
+    product_name: string;
+    product_description: string;
+    price: number;
+    quantity: number;
+    images: ProductImage[];
+}
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const useProduct = () => {
+    const path = useLocation();
+    const [product, setProduct] = useState<Product | null>(null)
+    const [error, setError] = useState<string>("")
+    const [valid, setValid] = useState<boolean>(false)
+    useEffect(() => {
+        const id = path.pathname.split('/').at(-1);
+        console.log(id);
+        try {
+            fetch(`${BACKEND_URL}/product/${id}`, {
+                method: 'GET',
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then(async (response: Response) => {
+                const data = await response.json()
+                setValid(data.valid);
+                if (!data || !data.valid) {
+                    setError(data.message);
+                } else {
+                    setProduct(data.product);
+                }
+            })
+        } catch (error) {
+            setError("Something went wrong")
+        }
+    }, [])
+    return { product, error, valid }
+}
+
+export default useProduct;
