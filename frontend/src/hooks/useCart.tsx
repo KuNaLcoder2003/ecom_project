da7950cf50214db5da4e83388bcec96d41ae5027
Kunal Singh <kunalindia59@gmail.com>
2025-12-28 14:48:30 +0530

    Adding new variables to the Hook -> unavaailable

--- frontend/src/hooks/useCart.tsx
+++ frontend/src/hooks/useCart.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import getUnavailable from "../functions/getUnavailable";
+import toast from "react-hot-toast";
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 interface ProductImage {
     id: string;
@@ -10,12 +12,20 @@ interface Cart {
     product_id: string;
     qunatity: number;
     price: number;
-    images: ProductImage[]
+    images: ProductImage[],
+    err?: string
+}
+interface Response_Unavailable {
+    product_id: string,
+    err: string
 }
 const useCart = () => {
     const [cart, setCart] = useState<Cart[]>();
     const [loading, setLoading] = useState<boolean>(false);
+    const [loading_unavailable, setLoadingUnavailable] = useState<boolean>(false);
+    const [route, setRoute] = useState<boolean>(false);
     const [error, setError] = useState<string>("");
+    const [unavailble, setUnavailable] = useState<Response_Unavailable[]>();
     useEffect(() => {
         setLoading(true);
         const token = localStorage.getItem('token')
@@ -41,7 +51,41 @@ const useCart = () => {
             setError("Something went wrong")
         }
     }, [])
-    return { cart, loading, error }
+
+    const handleCheckout = async (cart: Cart[]) => {
+        setLoadingUnavailable(true);
+        try {
+            const unavailble = await getUnavailable(cart)
+            if (!unavailble || unavailble == null) {
+                setUnavailable([]);
+                toast.error("Unable to checkout");
+                setRoute(false);
+            } else if (unavailble.length == 0) {
+                setUnavailable([]);
+                toast.error("Unable to checkout");
+                setRoute(false);
+            } else {
+                setUnavailable(unavailble);
+                const updated_cart = cart.map(item => {
+                    let temp = { ...item }
+                    unavailble.map(obj => {
+                        if (obj.product_id == item.product_id) {
+                            temp.err = obj.err
+                        }
+                    })
+                    return temp
+                })
+                setCart(updated_cart)
+                setRoute(true);
+            }
+        } catch (error) {
+            setUnavailable([]);
+            toast.error("Unable to checkout");
+            setRoute(false);
+        }
+    }
+    return { cart, loading, error, unavailble, route, handleCheckout, loading_unavailable }
+
 }
 
 export default useCart;
