c6b8a218ad3508af725c1df69ca66b89ff4dfd60
Kunal Singh <kunalindia59@gmail.com>
2025-12-28 14:50:06 +0530

    api route for orders

--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -1,4 +1,19 @@
 import express from "express";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '@prisma/client';
+import authMiddleware from "../middlewares/authMiddleware.js";
+const connectionString = `${process.env.DATABASE_URL}`
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter });
 const orderRouter = express.Router();
+
+orderRouter.post('/cart', authMiddleware, async (req: express.Request, res: express.Response) => {
+    // THIS ROUTE IS FOR ORDERING FROM CART
+    try {
+        const userId = "req.userId"
+    } catch (error) {
+
+    }
+})
 
 export default orderRouter;
