7a74c44677e351e0a2332756fb8cf74593338b51
Kunal Singh <kunalindia59@gmail.com>
2026-01-01 23:01:36 +0530

    Improving PaymentPay component api calling logic

--- frontend/src/pages/PaymentPage.tsx
+++ frontend/src/pages/PaymentPage.tsx
@@ -2,17 +2,30 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "../components/CheckoutForm";
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 const stripePromise = loadStripe(
     import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
 );
-
-function CheckoutPage({ orderId }: any) {
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+function PaymentPage() {
     const [clientSecret, setClientSecret] = useState<string>("");
+    const path = useLocation();
 
     useEffect(() => {
-        fetch(`/payAndConfirm/${orderId}`, { method: "POST" })
+        const orderId = path.pathname.split('/').at(-1);
+        const token = localStorage.getItem('token') as string;
+        fetch(`${BACKEND_URL}/order/payAndConfirm/${orderId}`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': token
+            },
+        })
             .then(res => res.json())
-            .then(data => setClientSecret(data.clientSecret));
+            .then(data => {
+                setClientSecret(data.clientSecret)
+                console.log(data);
+            });
     }, []);
 
     return (
@@ -23,4 +36,4 @@ function CheckoutPage({ orderId }: any) {
         )
     );
 }
-export default CheckoutPage;
+export default PaymentPage;
