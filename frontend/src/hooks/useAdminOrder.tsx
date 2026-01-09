a779121585b72e032d70274026c251d26ba19d4f
Kunal Singh <kunalindia59@gmail.com>
2026-01-10 01:28:53 +0530

    function to fetch ordered products

--- frontend/src/hooks/useAdminOrder.tsx
+++ frontend/src/hooks/useAdminOrder.tsx
@@ -28,12 +28,19 @@ interface Orders {
     cart_id: string;
     address_id: string;
 }
+interface MergedItem {
+    images: string[];
+    price: number;
+    qunatity: number; // keeping typo as-is to match backend
+    product: Product;
+}
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 function useAdminOrder() {
     const [orders, setOrders] = useState<Orders[]>();
-    // const [ordered_products, setOrderdProducts] = useState<OrderItem[]>()
+    const [ordered_products, setOrderdProducts] = useState<MergedItem[]>()
     const [loading, setLoading] = useState<boolean>(false);
+    const [loading_detail, setLoadingDetail] = useState<boolean>(false);
     const [error, setError] = useState<string>("");
     useEffect(() => {
         try {
@@ -60,6 +67,29 @@ function useAdminOrder() {
             setError(`${error}`)
         }
     }, [])
-    return { orders, error, loading }
+
+    async function getOrderdProduct(order_id: string) {
+        try {
+            setLoadingDetail(true)
+            const token = localStorage.getItem('token') as string
+            const reponse = await fetch(`${BACKEND_URL}/order/getOrderDetails/${order_id}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': token
+                }
+            })
+            const data = await reponse.json();
+            if (data.valid) {
+                setOrderdProducts(data.ordered_products);
+                setLoadingDetail(false)
+            } else {
+                setLoadingDetail(false)
+            }
+        } catch (error) {
+            setLoadingDetail(false)
+        }
+    }
+    return { orders, error, loading, ordered_products, getOrderdProduct, loading_detail }
 }
 export default useAdminOrder;
