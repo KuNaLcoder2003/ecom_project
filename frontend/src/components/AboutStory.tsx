0b680696ccedff89457d8da991f37c1b1af002b8
Kunal Singh <kunalindia59@gmail.com>
2026-01-13 22:56:26 +0530

    New component

---
+++ frontend/src/components/AboutStory.tsx
@@ -0,0 +1,20 @@
+const AboutStory = () => {
+    return (
+        <section className="max-w-5xl mx-auto mt-32 text-center px-6">
+            <p className="text-sm text-stone-500 mb-3">About Us</p>
+            <h2 className="text-4xl font-bold mb-4">
+                Creating style while redefining comfort
+            </h2>
+            <p className="text-stone-500 max-w-xl mx-auto mb-6">
+                We weave more than just fabric; we craft a narrative of redefine comfort
+                for the modern man.
+            </p>
+
+            <button className="px-6 py-3 bg-orange-500 text-white rounded-full">
+                Explore Now →
+            </button>
+        </section>
+    );
+};
+
+export default AboutStory;
