48d8c5a357f032d58b5c36532ef9ccb43546ca2b
Kunal Singh <kunalindia59@gmail.com>
2026-01-13 22:55:16 +0530

    New component

---
+++ frontend/src/components/CtaSections.tsx
@@ -0,0 +1,14 @@
+const CtaSection = () => {
+    return (
+        <section className="mt-32 bg-black text-white py-12 px-6">
+            <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center gap-6">
+                <h2 className="text-2xl font-semibold">
+                    Want to Buy For Your Shop? Call Now 👉
+                </h2>
+                <span className="text-lg font-mono">01789-217791</span>
+            </div>
+        </section>
+    );
+};
+
+export default CtaSection;
