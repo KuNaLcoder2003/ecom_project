d845cbefa3ba48e27783daa51cb9ce5ad243151d
Kunal Singh <kunalindia59@gmail.com>
2026-01-13 22:51:47 +0530

    New route for word wise serach

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,23 +1,38 @@
-import type React from "react";
 import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import Features from "../components/Features";
-import BestSellers from "../components/BestSellers";
-import Featured from "../components/Featured";
+import Hero from "../components/Hero"
+import { FabricTabs } from "../components/Hero";
+import { NewestFabrics } from "../components/Hero";
+import AboutStory from "../components/AboutStory";
+import PopularFabrics from "../components/PopularFabrics";
+import BestCategory from "../components/BestCategory";
+import InstagramGrid from "../components/Instagramgrid";
+import CtaSection from "../components/CtaSections";
 import Footer from "../components/Footer";
 
-const Home: React.FC = () => {
+const Home = () => {
     return (
-        <div className="w-screen h-auto overflow-x-hidden">
-            <Navbar />
-            <Hero show_blob={true} image_src="./assets/Hero-Image.png" image_class="w-[300px]" title="Fresh Arrivals Online" content="Discover Our Newest Collection Now" />
-            <Features />
-            <BestSellers />
-            <Hero show_blob={false} class_name="mt-60" image_src="./assets/Category-Image.png" image_class="w-[300px]" title="Browse Our Fashion Paradise!" content="Step into a world of style and explore our diverse collection of clothing categories" />
-            <Featured />
+        <div className="w-full overflow-x-hidden">
+            <Navbar word="" />
+
+            <Hero />
+
+            <FabricTabs />
+
+            <NewestFabrics />
+
+            <AboutStory />
+
+            <PopularFabrics />
+
+            <BestCategory />
+
+            <InstagramGrid />
+
+            <CtaSection />
+
             <Footer />
         </div>
-    )
-}
+    );
+};
 
 export default Home;
