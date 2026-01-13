db8b1947d70570fd933b4d1339bd415f5e814129
Kunal Singh <kunalindia59@gmail.com>
2026-01-13 22:56:47 +0530

    Design changes

--- frontend/src/components/ProductPreview.tsx
+++ frontend/src/components/ProductPreview.tsx
@@ -2,33 +2,34 @@ import type React from "react";
 import { useNavigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 
-interface ProductPreviewProps {
-    image_url: string,
-    product_name: string,
-    product_price: string
+interface Props {
+    image_url: string;
+    product_name: string;
+    product_price: string;
 }
-const ProductPreview: React.FC<ProductPreviewProps> = ({ image_url, product_name, product_price }) => {
+
+const ProductPreview: React.FC<Props> = ({ image_url, product_name, product_price }) => {
     const navigate = useNavigate();
     const { isLoggedIn } = useAuth();
+
     return (
-        <div onClick={() => {
-            if (isLoggedIn) {
-                navigate("/product/1")
-            } else {
-                navigate("/signin")
-            }
-        }} className="w-[264px] h-[434px] p-2 cursor-pointer">
-            <div className="w-full h-full space-y-4">
-                <div className="bg-stone-200 flex items-center justify-center w-full h-full">
-                    <img src={image_url} />
-                </div>
-                <div className="flex flex-col gap-2 items-baseline font-[InterSerif]">
-                    <p className="text-lg font-semibold">{product_name}</p>
-                    <p className="text-md font-light">{product_price}</p>
-                </div>
+        <div
+            onClick={() => navigate(isLoggedIn ? "/product/1" : "/signin")}
+            className="group cursor-pointer"
+        >
+            <div className="rounded-2xl overflow-hidden bg-stone-100 h-[320px] flex items-center justify-center">
+                <img
+                    src={image_url}
+                    className="group-hover:scale-105 transition-transform"
+                />
+            </div>
+
+            <div className="mt-4">
+                <p className="font-semibold">{product_name}</p>
+                <p className="text-stone-500">{product_price}</p>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default ProductPreview;
