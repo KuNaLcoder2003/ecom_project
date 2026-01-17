34c278ffb20b772e6e73376e323197993d363282
Kunal Singh <kunalindia59@gmail.com>
2026-01-17 16:54:19 +0530

    redesigning news letter

--- frontend/src/components/NewsLetter.tsx
+++ frontend/src/components/NewsLetter.tsx
@@ -2,32 +2,22 @@ import type React from "react";
 
 const NewsLetter: React.FC = () => {
     return (
-        <div className={`w-screen h-[230px] bg-stone-100 flex items-center`}>
-            <div className="flex relative w-full justify-around items-center h-full z-10 p-4">
-                <div className="flex flex-col items-baseline gap-12 z-20 justify-center">
-                    <div className="flex flex-col items-baseline justify-center">
-                        <h2 className="text-4xl font-bold font-[InterSerif]">Join Our NewsLetter</h2>
-                        <p className="text-stone-400 font-[InterSerif] text-sm w-[70%]">We love to surprise our subscribers with occasional gifts.</p>
-                    </div>
-
-                    <button className="text-white bg-black p-2 px-4 rounded-lg cursor-pointer">{"View Collection   ->"}</button>
-                </div>
-
-                <div className="flex items-center gap-4">
-                    <div className="w-full max-w-2xl">
-                        <div className="relative">
-                            <input
-                                type="text"
-                                placeholder="Enter Your Email..."
-                                className="w-full pl-4 pr-4 py-3 border border-stone-400 rounded-lg text-gray-900 placeholder-stone-400 focus:outline-none focus:ring-2 focus:ring-stone-400 focus:border-transparent"
-                            />
-                        </div>
-                    </div>
-                    <div className="p-2">
-                        <button className="p-2 px-4 text-white bg-black rounded-lg cursor-pointer">Subscribe</button>
+        <div className="max-w-7xl m-auto flex items-center mt-10 h-[400px] mb-10">
+            <div className="w-full p-10 flex items-center bg-gray-100 h-full rounded-l-lg">
+                <div className="flex flex-col items-baseline gap-2">
+                    <h3 className="text-4xl font-[Interif] font-medium">Join our newsletter. <br /> Enjoy big discounts.</h3>
+                    <p className="text-lg font-[Interif] font-light">Hear what they say about us</p>
+                    <div className="mt-6 flex items-center gap-4">
+                        <input
+                            className="w-[100%] bg-gray-100 rounded-lg px-6 py-2 font-[Interif] border border-zinc-300"
+                            placeholder="example@gmail.com"
+                        />
+                        <button className="px-4 py-2 text-white bg-black text-center rounded-lg font-[Interif]">SignUp</button>
                     </div>
                 </div>
-
+            </div>
+            <div className="w-full rounded-top-lg h-full">
+                <img src="https://framerusercontent.com/images/aS0CKio804CAynX0og6pE47HiZ4.webp?scale-down-to=2048" className="object-cover rounded-r-lg h-full" />
             </div>
         </div>
     )
