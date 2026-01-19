2e15832a880f3b611dad1c4d5ef3848214cabd7b
Kunal Singh <kunalindia59@gmail.com>
2026-01-19 22:51:59 +0530

    Adding variant id for fetching products and fixing bugs releated to product_varaint

--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -1,15 +1,45 @@
 import express from "express";
 import { PrismaPg } from '@prisma/adapter-pg'
-import { cart_products, PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import authMiddleware from "../middlewares/authMiddleware.js";
 import Stripe from "stripe";
-import { success } from "zod";
 import adminMiddleware from "../middlewares/adminMiddleware.js";
 const connectionString = `${process.env.DATABASE_URL}`
 const adapter = new PrismaPg({ connectionString })
 const prisma = new PrismaClient({ adapter });
 const orderRouter = express.Router();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+
+interface ProductType {
+    id: string;
+    product_name: string;
+    product_description: string;
+}
+interface ProductVariant {
+    id: string;
+    color: string;
+    size: string;
+    price: number;
+    quantity: number
+    product: ProductType
+}
+
+interface ProductImage {
+    id: string
+    product_id: string;
+    image_url: string;
+}
+
+interface ProductObj {
+    id: string;
+    color: string;
+    size: string;
+    price: number;
+    quantity: number
+    product: ProductType;
+    images: ProductImage[]
+}
 orderRouter.post('/cart', authMiddleware, async (req: express.Request, res: express.Response) => {
     // THIS ROUTE IS FOR ORDERING FROM CART
     try {
@@ -68,24 +98,65 @@ orderRouter.post('/createOrder', authMiddleware, async (req: any, res: express.R
                 }
             })
             const product_ids = cart_products.map(item => {
-                return item.product_id
+                return item.product_variant_id
+            })
+            const products = await tx.product_variants.findMany({
+                where: {
+                    id: {
+                        in: product_ids
+                    }
+                },
+                select: {
+                    size: true,
+                    color: true,
+                    id: true,
+                    product: true
+                }
+            })
+            const images = await tx.product_images.findMany({
+                where: {
+                    product_id: {
+                        in: products.map(item => {
+                            return item.product.id
+                        })
+                    }
+                }
             })
             if (!cart_products) {
                 throw new Error("Bad request")
             }
-
-
-
-            const lineItems = cart_products.map(item => {
+            const merged_arr = products.map((item) => {
+                let temp: ProductVariant = { ...item, price: 0, quantity: 0 }
+                cart_products.map((obj) => {
+                    if (obj.product_variant_id == item.id) {
+                        temp["price"] = obj.price
+                        temp["quantity"] = obj.qunatity
+                    }
+                })
+                return temp;
+            })
+            const final_arr = merged_arr.map((item) => {
+                const temp: ProductObj = { ...item, images }
+                images.map((obj) => {
+                    if (obj.product_id == item.product.id) {
+                        temp["images"] = [...temp["images"], obj]
+                    }
+                })
+                return temp;
+            })
+            console.log("Final Arr : ", final_arr)
+            const lineItems = final_arr.map(item => {
                 return {
                     price_data: {
                         currency: 'inr',
                         product_data: {
-                            name: item.product_id,
+                            name: item.product.product_name,
+                            description: item.product.product_name,
+                            images: [item.images[0].image_url]
                         },
                         unit_amount: item.price * 100,
                     },
-                    quantity: item.qunatity,
+                    quantity: item.quantity,
                 }
             })
 
