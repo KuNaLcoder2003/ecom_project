5dd53fb8e57acbdd465c7cc32aaa9f1f81f60a6c
Kunal Singh <kunalindia59@gmail.com>
2026-01-24 13:15:31 +0530

    Adding a loader

--- frontend/src/components/Addresses.tsx
+++ frontend/src/components/Addresses.tsx
@@ -1,6 +1,8 @@
 import toast, { Toaster } from "react-hot-toast";
 import useCart from "../hooks/useCart";
 import { loadStripe } from "@stripe/stripe-js";
+import { useState } from "react";
+import { LoaderPinwheel } from "lucide-react";
 
 type Address = {
     id: string;
@@ -21,6 +23,7 @@ const AddressStep = ({
     onSelect: any;
 }) => {
     const { cart } = useCart();
+    const [loading, setLoading] = useState<boolean>(false)
     const handleAddress = async () => {
 
         if (!selectedAddress) {
@@ -34,6 +37,7 @@ const AddressStep = ({
         const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
         const token = localStorage.getItem('token') as string
         try {
+            setLoading(true);
             const response = await fetch(`${BACKEND_URL}/order/createOrder`, {
                 method: 'POST',
                 headers: {
@@ -52,55 +56,65 @@ const AddressStep = ({
                     toast.error("Stripe failed to load");
                     return;
                 }
+                setLoading(false);
                 window.location.href = data.session_url;
             } else {
                 toast.error(data.message);
+                setLoading(false);
             }
         } catch (error) {
             toast.error("Something went wrong")
+            setLoading(false);
         }
     }
     return (
         <>
             <Toaster />
-            <div className="bg-white rounded-2xl p-6 shadow-sm space-y-6">
+            {
+                loading ? <div className="w-screen h-screen flex items-center justify-center">
+                    <div className="flex flex-col items-center justify-center gap-4">
+                        <h3 className="text-3xl font-semibold font-[Interif] text-center">Please wait we're setting up payment</h3>
+                        <LoaderPinwheel />
+                    </div>
+                </div> : <div className="bg-white rounded-2xl p-6 shadow-sm space-y-6">
 
-                <h2 className="text-2xl font-semibold font-[Interif]">
-                    Select Delivery Address
-                </h2>
+                    <h2 className="text-2xl font-semibold font-[Interif]">
+                        Select Delivery Address
+                    </h2>
 
-                <div className="space-y-4">
-                    {addresses.map((addr: any) => {
-                        const active = selectedAddress?.id === addr.id
-                        return (
-                            <div
-                                key={addr.id}
-                                onClick={() => onSelect(addr)}
-                                className={`p-4 rounded-xl cursor-pointer border transition
+                    <div className="space-y-4">
+                        {addresses.map((addr: any) => {
+                            const active = selectedAddress?.id === addr.id
+                            return (
+                                <div
+                                    key={addr.id}
+                                    onClick={() => onSelect(addr)}
+                                    className={`p-4 rounded-xl cursor-pointer border transition
                             ${active ? "border-black bg-zinc-50" : "border-stone-100 hover:border-zinc-300"}`}
-                            >
-                                <p className="font-medium">
-                                    {addr.house_no}, {addr.street}
-                                </p>
-                                <p className="text-sm text-zinc-500">
-                                    {addr.city}, {addr.state} - {addr.pin_code}
-                                </p>
-                            </div>
-                        )
-                    })}
-                </div>
+                                >
+                                    <p className="font-medium">
+                                        {addr.house_no}, {addr.street}
+                                    </p>
+                                    <p className="text-sm text-zinc-500">
+                                        {addr.city}, {addr.state} - {addr.pin_code}
+                                    </p>
+                                </div>
+                            )
+                        })}
+                    </div>
 
-                <button
-                    onClick={async () => await handleAddress()}
-                    disabled={!selectedAddress}
-                    className={`w-full py-3 rounded-xl transition
+                    <button
+                        onClick={async () => await handleAddress()}
+                        disabled={!selectedAddress}
+                        className={`w-full py-3 rounded-xl transition
                 ${selectedAddress
-                            ? "bg-black text-white"
-                            : "bg-zinc-200 text-zinc-400 cursor-not-allowed"}`}
-                >
-                    Place Order
-                </button>
-            </div>
+                                ? "bg-black text-white"
+                                : "bg-zinc-200 text-zinc-400 cursor-not-allowed"}`}
+                    >
+                        Place Order
+                    </button>
+                </div>
+            }
         </>
     );
 };
